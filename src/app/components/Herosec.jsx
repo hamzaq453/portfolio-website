3c19c0160f7e712cac94e5721fbe3dd8231e207f
Hamza <qhamza4532@gmail.com>
2024-01-09 17:29:06 +0500

    uuup

--- src/app/components/Herosec.jsx
+++ src/app/components/Herosec.jsx
@@ -6,7 +6,7 @@ const Herosec = () => {
     <section>
         <div className='grid grid-cols-1 lg:grid-cols-12'>
             <div className='col-span-7 place-self-center'>
-        <h1 className='text-white mb-4 text-4xl font-extrabold lg:text-6xl'>Hello, I'm Hamza </h1>
+        <h1 className='text-white mb-4 text-4xl font-extrabold lg:text-6xl'>Hello! I am Hamza </h1>
         
         <p className='text-[#ADB7BE] text-lg lg:text-xl mb-4'>
             Lorem isspm jdnes wokejs jxnjws kxx ks  c cksnkwnxksxn csc skckc
